292d5678dbc7d10686791423bb62f820d9d17c5e
Alex <connectionwithme1@gmail.com>
2023-11-23 12:36:09 +0200

    fix start v 35

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::get('/', function () {
 });
 
 Route::get('/contacts', function () {
+    $contacts = [
+        1 => ['name' => 'Name 1', 'phone' => '1234567890'],
+        2 => ['name' => 'Name 2', 'phone' => '2345678901'],
+        3 => ['name' => 'Name 3', 'phone' => '3456789012'],
+    ];
     return view('contacts.index');
 })->name('contacts.index');
 
